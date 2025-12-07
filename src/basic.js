6ef33969a8f1402a0821b654865afb085f9011ab
gzb <gaspardzebear@hotmail.fr>
2025-12-07 16:24:01 +0100

    refactoring, works

--- src/basic.js
+++ src/basic.js
@@ -4,7 +4,7 @@ import { createCamera } from "./components/camera";
 import { createScene } from "./components/scene";
 import { texture } from "three/tsl";
 import { createRenderer } from "./components/renderer";
-import ThingMesh from "./classes/ThingMesh";
+import ThingMesh from "./classes/ThingMesh.js";
 
 console.log("Entering basic.js")
 const renderer= createRenderer()
@@ -12,52 +12,25 @@ const renderer= createRenderer()
 const camera = createCamera()
 camera.position.set(0, 0, 50);
 const scene = createScene('black')
-// Point zero
-//const zeroGeometry = new THREE.IcosahedronGeometry(3, 1)
-//const zeroMaterial = new THREE.MeshPhongMaterial({
-//  color: 0xffaa00,
-//  transparent: false,
-//  opacity: 1,
-//  depthTest: false,
-//  depthWrite: true,
-//});
-//const zeroMesh = new THREE.Mesh(zeroGeometry, zeroMaterial);
-
-const zeroMesh = new ThingMesh()
-
-// Ajout fils de fer
-//const wireMat = new THREE.MeshBasicMaterial({
-//  color: 0xffffff,
-//  wireframe: true
-//})
-//const wireMesh = new THREE.Mesh(zeroMesh.getGeometry(), wireMat)
-//zeroMesh.addMesh(wireMesh)
-//scene.add(wireMesh)
-scene.add(zeroMesh)
+const mesh0 = new ThingMesh('alia00.jpg')
+const mesh1 = new ThingMesh('alia01.jpg')
+const mesh2 = new ThingMesh('alia02.jpg')
+scene.add(mesh0)
+scene.add(mesh1)
+scene.add(mesh2)
 
 // Ajouter une lumière ambiante
-const hemiLight = new THREE.HemisphereLight(0xaa5500, 0x0099ff);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
 scene.add(hemiLight);
 
-let x = 0
-let y = 0
-let z = 0
-let orbit=25
-let angle = 0;
-zeroMesh.position.x = orbit * Math.cos(angle);
-      //sphere.position.y = orbit * Math.sin(angle);
-zeroMesh.position.z = orbit * Math.sin(angle);
+mesh0.initPosition(0,-5,0,25,0)
+mesh1.initPosition(0,0,0,10,Math.PI)
+mesh2.initPosition(0,7,0,30,Math.PI/2)
 function animate(t) {
   requestAnimationFrame(animate);
-  angle += 0.01
-  //zeroMesh.position.x += 0.01
-  zeroMesh.position.x = orbit * Math.cos(angle);
-  //zeroMesh.position.y += 1
-  zeroMesh.position.z = orbit*Math.sin(angle)
-  //zeroMesh.position.set(x,y,z)
-  zeroMesh.rotateX(Math.PI / 300)
-  zeroMesh.rotateY(Math.PI / 300)
-  zeroMesh.rotateZ(Math.PI / 100)
+  mesh0.move()
+  mesh1.move()
+  mesh2.move()
   renderer.render(scene, camera)
 }
 
--- src/classes/ThingMesh.js
+++ src/classes/ThingMesh.js
@@ -1,46 +1,65 @@
-import { 
-    IcosahedronGeometry, 
-    MeshPhongMaterial,
-    MeshBasicMaterial,
-    Mesh,
-    } from 'three';
+import {
+  IcosahedronGeometry,
+  MeshPhongMaterial,
+  MeshBasicMaterial,
+  Mesh,
+  TextureLoader,
+} from 'three';
 
 class ThingMesh extends Mesh {
 
-  constructor() {
+  constructor(zTexture) {
     super()
-    this.geometry = new IcosahedronGeometry(3, 1)
+    this.geometry = new IcosahedronGeometry(7, 10)
+    const texture = new TextureLoader().load('textures/' + zTexture)
     this.material = new MeshPhongMaterial({
-      color: 0xffaa00,
+      map: texture,
+      color: 0xffffff,
       transparent: true,
-      opacity: 0.5,
-      depthTest: false,
-      depthWrite: true,
+      opacity: 0.99,
+      //depthTest: false,
+      //depthWrite: true,
     });
     this.mesh = new Mesh(this.geometry, this.material);
     // Ajout fils de fer
     const wireMat = new MeshBasicMaterial({
-      color: 0xffffff,
+      color: 0xff00ff,
       wireframe: true
     })
     const wireMesh = new Mesh(this.geometry, wireMat)
-  }
-
-  getMesh() {
-    return(this.mesh)
+    //this.add(wireMesh)
   }
 
   getGeometry() {
-    return(this.geometry)
+    return (this.geometry)
   }
 
   getMaterial() {
-    return(this.material)
+    return (this.material)
   }
 
-  addMesh(mesh) {
-    this.mesh.add(mesh)
+  initPosition(x, y, z, orbit, angle) {
+    this.orbit = orbit
+    this.angle = angle;
+    this.x0=x
+    this.y0=y
+    this.z0=z
+    this.move()
+    //this.mesh.position.x = orbit * Math.cos(angle);
+    //this.mesh.position.y = y
+    //this.mesh.position.z = orbit * Math.sin(angle);
   }
-    
+
+  move() {
+    this.angle += 0.001
+    //zeroMesh.position.x += 0.01
+    this.position.x = this.orbit * Math.cos(this.angle);
+    this.position.y = this.y0
+    this.position.z = this.orbit * Math.sin(this.angle)
+    //zeroMesh.position.set(x,y,z)
+    //this.mesh.rotateX(Math.PI / 300)
+    this.rotateY(Math.PI / 300)
+    this.rotateZ(Math.PI / 600)
+    }
 }
 export default ThingMesh

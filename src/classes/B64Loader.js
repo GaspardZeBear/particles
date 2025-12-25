e11e3d811f5c32f44d05f558a8272fde7802c38a
gzb <gaspardzebear@hotmail.fr>
2025-12-25 15:39:51 +0100

    message on load

--- src/classes/B64Loader.js
+++ src/classes/B64Loader.js
@@ -6,16 +6,18 @@ import { TextureLoader } from 'three';
 class B64Loader {
     static b64 = true
     b64load(src) {
+        console.log("b64 ", B64Loader.b64 , "loading texture ", src )
         const textureLoader = new TextureLoader();
         let texture=null
         if (B64Loader.b64) {
             texture = textureLoader.load(textures[src]);
             return (texture)
         } else {
-            texture = new TextureLoader().load('textures/' + src)
+            texture = textureLoader.load('textures/' + src)
         }
         //texture.wrapS=MirroredRepeatWrapping
         //texture.wrapT=MirroredRepeatWrapping
+        console.log("b64 ", B64Loader.b64 , "texture ", src , " is ", texture )
         return (texture)
         //const material = new THREE.MeshBasicMaterial({ map: texture });
     }

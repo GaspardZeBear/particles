734518e4c1105d2b9aad7c7d61d60fe39a28743c
gzb <gaspardzebear@hotmail.fr>
2026-01-08 22:39:42 +0100

    Adding _defaults values

--- src/params/BasicParams.mjs
+++ src/params/BasicParams.mjs
@@ -4,7 +4,7 @@ class BasicParams {
     static P
     static profile
     static {
-        console.log("static")
+        console.log("Applying ./BasicParamsConf.mjs")
         BasicParams.P=conf
     }
 
@@ -15,10 +15,18 @@ class BasicParams {
     }
 
     static setProfile(profile) {
-        return(BasicParams.profile=profile)
+        //return(BasicParams.profile=profile)
+        for (let d of Object.keys(BasicParams.P["_defaults"])) {
+            if ( BasicParams.P[profile][d] === undefined ) {
+               console.log(`setting param ${d} from _defaults, value ${BasicParams.P["_defaults"][d]}`)
+               BasicParams.P[profile][d] = BasicParams.P["_defaults"][d]
+            }
+        }
+        BasicParams.profile=profile
     }
 
     static getProfile(profile) {
+
         return(BasicParams.P[BasicParams.profile])
     }
 
